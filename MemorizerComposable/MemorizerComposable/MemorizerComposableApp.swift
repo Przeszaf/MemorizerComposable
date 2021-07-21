e36726cf88800b970339c021f6ad2d5ca6f6d77b
Przemyslaw Szafulski <>
2021-07-21 16:56:55 +0100

    Add initial, empty project

---
+++ MemorizerComposable/MemorizerComposable/MemorizerComposableApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizerComposableApp.swift
+//  MemorizerComposable
+//
+//  Created by Przemyslaw Szafulski on 21/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizerComposableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
