42a79832ad4c7e0ce8bad18822e5402039b3ff60
Przemyslaw Szafulski <>
2021-07-28 19:27:47 +0100

    Lesson 7 - Implicit animations

--- MemorizerComposable/MemorizerComposable/MemoryGame/UI/CardView.swift
+++ MemorizerComposable/MemorizerComposable/MemoryGame/UI/CardView.swift
@@ -16,20 +16,26 @@ struct CardView: View {
                 Pie(startAngle: Angle(degrees: 0 - 90), endAngle: Angle(degrees: 120 - 90))
                     .padding(DrawingConstants.circlePadding)
                     .opacity(DrawingConstants.circleOpacity)
-                Text(card.content).foregroundColor(.black).font(font(in: geometry.size))
+                Text(card.content)
+                    .rotationEffect(Angle(degrees: card.isMatched ? 360 : 0))
+                    .animation(.linear(duration: 1).repeatForever(autoreverses: false))
+                    .font(.system(size: DrawingConstants.fontSize))
+                    .scaleEffect(scale(thatFits: geometry.size))
             }
             .cardify(isFaceUp: card.isFaceUp)
+            
         }
     }
-
-    private func font(in size: CGSize) -> Font {
-        .system(size: min(size.width, size.height) * DrawingConstants.fontScale)
+    
+    private func scale(thatFits size: CGSize) -> CGFloat {
+        min(size.width, size.height) / (DrawingConstants.fontSize / DrawingConstants.fontScale)
     }
 
     private enum DrawingConstants {
         static let fontScale: CGFloat = 0.7
         static let circleOpacity: Double = 0.5
         static let circlePadding: CGFloat = 5
+        static let fontSize: CGFloat = 32
     }
 }
 
